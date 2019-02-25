05ba48278bd778221e3963356080c8a4c96db5f2
Vanellope Wang <35799837+Vanellope0602@users.noreply.github.com>
2019-02-25 16:24:49 +0800

    Add files via upload

---
+++ APlusBOne.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class APlusBOne {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long x;
+        long y;
+        x = s.nextLong();
+        y = s.nextLong();
+        System.out.println(x + y);
+    }
+
+}
